776e8ceef1fbe373567c6b15e295072db8822bf4
Anthony Sutton <anthony.sutton95@gmail.com>
2019-12-04 13:29:10 -0800

    Built our postRoutes and middleware, except for put since it keeps blocking my body from being passed in

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,28 +1,68 @@
 const express = require('express');
 
 const router = express.Router();
+const Posts = require("./postDb");
+
+// custom middleware
+// Checks if the postID being passed through exists, if found, returns the object back so that it can be displayed to the client, else, displays 404 error
+function validatePostId(req, res, next) {
+  // do your magic!
+  Posts.getById(req.params.id)
+    .then(post => {
+      if (post) {
+        req.response = post;
+        next();
+      } else {
+        res.status(400).json({message: "The specified post ID does not exist!!!"})
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
 
 router.get('/', (req, res) => {
   // do your magic!
-  res.send('from posts');
+  Posts.get()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({error: "Unable to retrieve posts"});
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, (req, res) => {
   // do your magic!
+  Posts.getById(req.response.id)
+    .then(response => {
+      // Checks if a post response is sent back and displays the response, else, returns a 404
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({error: "Unable to retrieve posts."});
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {
   // do your magic!
+  const postToBeDeleted = [{...req.response}];
+
+  Posts.remove(req.response.id)
+    .then(deletedPost => {
+      res.status(200).json(postToBeDeleted);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({error: "Unable to delete the specified post."});
+    });
+
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+  res.send('Still working')
 });
-
-// custom middleware
-
-function validatePostId(req, res, next) {
-  // do your magic!
-}
 
 module.exports = router;
