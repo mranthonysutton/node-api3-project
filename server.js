0c8739126d676c6597f62679b18b20709ef064f5
Anthony Sutton <anthony.sutton95@gmail.com>
2019-12-04 12:06:14 -0800

    Setup basic server template and imported routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const server = express();
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
